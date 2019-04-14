11da05f915e7778799e01a16dde59302a0097ed3
Biswash Lamichhane <madridistabiswash@gmail.com>
2019-04-14 16:27:42 +0545

    Tax Calculator

---
+++ README.md
@@ -0,0 +1,9 @@
+1.  Preview as shown below :
+
+![Alt text](preview_first.JPG?raw=true "Preview First")
+
+![Alt text](preview_second.JPG?raw=true "Preview Second")
+
+
+
+
---
+++ app/src/main/java/com/netlify/helloapp/taxer/MainActivity.java
@@ -0,0 +1,56 @@
+package com.netlify.helloapp.taxer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtDisplaySalry;
+    Button btnCalculate;
+    EditText iptSalary;
+
+    Engine eng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtDisplaySalry = findViewById(R.id.txtSalaryAmount);
+
+
+        btnCalculate = findViewById(R.id.btnCalSalary);
+
+        iptSalary = findViewById(R.id.iptSalary);
+
+        eng = new Engine();
+
+
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String salary = iptSalary.getText().toString();
+
+                if (salary.isEmpty()){
+
+                    Toast.makeText(MainActivity.this, "Empty Fields", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    double tax = eng.OnePercent(Double.parseDouble(salary));
+                    txtDisplaySalry.setText(String.valueOf(tax));
+
+                }
+            }
+        });
+
+
+    }
+}
